7486dabfd14f0a119118d2704dfccf975878aebb
한승헌 <seungheon123@khu.ac.kr>
2023-07-01 21:55:35 +0900

    Express application configuration

---
+++ config/express.js
@@ -0,0 +1,23 @@
+import  express  from "express";
+import compression from "compression";
+import methodOverride from "method-override";
+import cors from "cors";
+import {sequelize} from "../models/index.js";
+
+const app = express();
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride());
+app.use(cors());
+
+
+export default app;
