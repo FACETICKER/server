5ebbc6d3ba2c259035c8d235b3ac3c8b4b352929
한승헌 <seungheon123@khu.ac.kr>
2023-07-02 22:47:34 +0900

    Add DB_ERROR SERVER_ERROR

--- config/baseResponse.js
+++ config/baseResponse.js
@@ -9,6 +9,10 @@ const baseResponse = {
     //Social Google
     SIGNUP_GOOGLE_EXUSER : {"isSuccess":true, "code":3000, "message":"이미 가입된 사용자입니다"},
     SIGNUP_GOOGLE_NEWUSER: {"isSUccess":true, "code":3001, "message":"새로운 사용자입니다"},
+
+    //Connection, Transaction 등의 서버 오류
+    DB_ERROR : { "isSuccess": false, "code": 4000, "message": "데이터 베이스 에러"},
+    SERVER_ERROR : { "isSuccess": false, "code": 4001, "message": "서버 에러"},
 };
 
 
