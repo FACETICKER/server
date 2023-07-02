cf8c7f25cd70b81d15f2f2cd3064c4a32be191cb
한승헌 <seungheon123@khu.ac.kr>
2023-07-02 22:50:27 +0900

    fixed kakao route

--- src/app/user/userRoute.js
+++ src/app/user/userRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
 import {handleKakaoCallback} from "./userController.js";
 
-const authRouter = express.Router();
+const userRouter = express.Router();
 
-authRouter.get('/kakao/callback',handleKakaoCallback); //카카오 로그인 경로
-
-
+userRouter.get('/auth/kakao/callback',handleKakaoCallback); //카카오 로그인 경로
 
 
 
-export default authRouter;
+
+
+export default userRouter;
