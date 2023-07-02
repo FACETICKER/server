7b7746ddaaeba74af67d0bdebe787127dcdec718
한승헌 <seungheon123@khu.ac.kr>
2023-07-02 22:49:17 +0900

    Fixed errors

--- src/app/user/userController.js
+++ src/app/user/userController.js
@@ -1,27 +1,41 @@
 import axios from "axios";
+import qs from "qs"
 import {createUser} from "./userService.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const handleKakaoCallback = async(req,res)=>{ 
     const code = req.query.code;
     try{
-        const accessTokenresponse = await axios.post('https://kauth.kakao.com/oauth/token',{ //accessToken 받기
-            grant_type: '#####',
-            client: '#####',
-            redirect_uri: '/auth/kakao/callback',
-            code: code, 
-        });
-        const accessToken = accessTokenresponse.data.accessToken;
-        const userInfoResponse = await axios.get('https://kapi.kakao.com/v2/user/me',{ //accessToken으로 사용자 정보 받아오기
+        const accessTokenResponse = await axios({
+            method: 'POST',
+            url: 'https://kauth.kakao.com/oauth/token',
             headers:{
-                Authorization:`Bearer${accessToken}`,
+                'content-type': 'application/x-www-form-urlencoded'
             },
+            data: qs.stringify({
+                grant_type: 'authorization_code',
+                client_id: process.env.KAKAO_ID,
+                redirect_uri: 'http://localhost:8001/auth/kakao/callback',
+                code: code,
+            })
         });
+        const accessToken = accessTokenResponse.data.access_token;
+        const userInfoResponse = await axios({
+            method: 'GET',
+            url:'https://kapi.kakao.com/v2/user/me',
+            headers:{
+                'Authorization':`Bearer ${accessToken}`,
+                'content-type': 'application/x-www-form-urlencoded'
+            }
+        })
         const userInfo = userInfoResponse.data; 
-        const Result = createUser(userInfo);
-
-        res.json(userInfo);
-    }catch{
-        console.error('Failed to process Kakao callback:', error.response.data);
-        res.status(500).json({ error: 'Failed to process Kakao callback' });
+        const provider = 'kakao';
+        const Result = await createUser(userInfo,provider);
+        console.log(Result);
+        return res.status(200).send(Result);
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to process Kakao callback' });
     }
 };
