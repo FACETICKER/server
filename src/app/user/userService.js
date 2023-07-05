95b10643b24c126456d97adfa2d48f79d2d28ef2
한승헌 <seungheon123@khu.ac.kr>
2023-07-05 16:45:08 +0900

    Refactor to use findUser and createUser functions

--- src/app/user/userService.js
+++ src/app/user/userService.js
@@ -1,47 +1,75 @@
 import User from "../../../models/user.js"
 import {response, errResponse} from "../../../config/response.js";
 import baseResponse from "../../../config/baseResponse.js";
+import { findUser, createUser } from "./userDao.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const createUser = async(userInfo, provider) =>{
+export const kakaoLogin = async(userInfo, provider) =>{
     try{
         //기존의 사용자인지 확인
-        const exUser = await User.findOne({
-            where:{
-                user_email:`${userInfo.kakao_account.email}`,
-                provider: `${provider}`,
-            }
-        })
+        const userInfoParams = {
+            email : `${userInfo.kakao_account.email}`,
+            provider: `${provider}`,
+        };
+        const exUser = await findUser(userInfoParams);
         if(exUser){
+            //토큰 발급
             let token = jwt.sign({
                 user_email : userInfo.kakao_account.email,
             },
             process.env.JWT_SECRET,{
                 expiresIn: "1h",
             });
-            console.log('---------------------------------');
-            console.log(token);
             return response(baseResponse.SIGNUP_KAKAO_EXUSER,{'jwt':token});
         }
         else{ //기존의 사용자가 아니면 새로운 사용자 추가
-            const newUser = await User.create({
-                user_email: `${userInfo.kakao_account.email}`,
-                user_name: `${userInfo.kakao_account.name}`,
-                user_image: `${userInfo.kakao_account.profile.profile_image_url}`,
-                provider: `${provider}`,
-                birthday: `${userInfo.kakao_account.birthday}`,
-            })
-            let token = await jwt.sign({
-                user_email : userInfo.kakao_account.email,
-            },
-            process.env.JWT_SECRET,{
-                expiresIn: "1h",
-
-            });
-            return response(baseResponse.SIGNUP_KAKAO_NEWUSER,{'jwt':token});
+            const newUser = await createUser(userInfoParams);
+            //토큰 발급
+            if(newUser){
+                let token = jwt.sign({
+                    user_email : userInfo.kakao_account.email,
+                },
+                process.env.JWT_SECRET,{
+                    expiresIn: "1h",
+                });
+                return response(baseResponse.SIGNUP_KAKAO_NEWUSER,{'jwt':token});
+            };
         }
+    }catch(err){
+        console.error(err);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+}
+
+
+export const googleLogin = async(userInfo, provider) =>{
+    try{
+        const userInfoParams = {
+            email : `${userInfo.email}`,
+            provider: `${provider}`,
+        };
+        const exUser = await findUser(userInfoParams);
+        if(exUser){
+            let token = jwt.sign({
+                user_email: userInfo.email,
+            },process.env.JWT_SECRET,{
+                expiresIn: "1h",
+            });
+            return response(baseResponse.SIGNUP_GOOGLE_EXUSER,{'jwt':token});
+        }
+        else{
+            const newUser = await createUser(userInfoParams);
+            if(newUser){
+                let token = jwt.sign({
+                    user_email: userInfo.email,
+                },process.env.JWT_SECRET,{
+                    expiresIn: "1h",
+                });
+                return response(baseResponse.SIGNUP_GOOGLE_NEWUSER,{'jwt':token});
+            };
+        };
     }catch(err){
         console.error(err);
         return errResponse(baseResponse.DB_ERROR);
