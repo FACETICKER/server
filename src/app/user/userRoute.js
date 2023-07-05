32cd6dd07a7129713be9325383f951be1f1f0a74
한승헌 <seungheon123@khu.ac.kr>
2023-07-05 20:32:16 +0900

    Add userRouter.js

--- src/app/user/userRoute.js
+++ src/app/user/userRoute.js
@@ -1,4 +1,6 @@
 import express from "express";
-import {handleKakaoCallback, handleGoogleCallback} from "./userController.js";
+
+
 
 const userRouter = express.Router();
+export default userRouter;
