5e0c998d1cca12e371bdef7b5dec3d92f82222e2
한승헌 <seungheon123@khu.ac.kr>
2023-07-05 16:43:08 +0900

    Added Google login API endpoint

--- src/app/user/userRoute.js
+++ src/app/user/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {handleKakaoCallback} from "./userController.js";
+import {handleKakaoCallback, handleGoogleCallback} from "./userController.js";
 
 const userRouter = express.Router();
 
-userRouter.get('/auth/kakao/callback',handleKakaoCallback); //카카오 로그인 경로
+userRouter.get('/auth/kakao/callback',handleKakaoCallback); //카카오 로그인 API
+userRouter.get('/auth/google/callback',handleGoogleCallback); //구글 로그인 API
 
 
 
