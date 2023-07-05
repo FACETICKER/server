3d4f72683480a90cde4ff82fdbe77ae7a34babb8
한승헌 <seungheon123@khu.ac.kr>
2023-07-05 21:27:33 +0900

    Add kakao login endpoint

--- src/app/user/userRoute.js
+++ src/app/user/userRoute.js
@@ -1,6 +1,8 @@
 import express from "express";
-
-
+import { handleKakaoCallback } from "./userController";
 
 const userRouter = express.Router();
+userRouter.get('/auth/kakao/callback',handleKakaoCallback); //카카오 로그인 API
+
+
 export default userRouter;
