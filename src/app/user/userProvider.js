dbd6f18606a4fbb9a72d2c40ac4ab2702aadd25c
한승헌 <seungheon123@khu.ac.kr>
2023-07-15 15:58:03 +0900

    Add: 사용자 확인 기능 추가

--- src/app/user/userProvider.js
+++ src/app/user/userProvider.js
@@ -2,7 +2,7 @@ import pool from "../../../config/database.js";
 import {findUser} from "./userDao.js";
 
 
-export const userCheck = async(userInfoParams) =>{ // 사용자 정보를 조회해서 사용자가 있으면 1 없으면 0을 반환
+export const userCheck = async(userInfoParams) =>{ // 사용자 정보를 조회
     try{
         const connection = await pool.getConnection(async conn => conn);
         const userCheckResult = await findUser(connection,userInfoParams);
