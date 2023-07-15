a252bd79d215d4902efafea39c16ac1d49879161
한승헌 <seungheon123@khu.ac.kr>
2023-07-15 15:52:50 +0900

    Update: DDL로 수정

--- src/app/user/userDao.js
+++ src/app/user/userDao.js
@@ -1,19 +1,20 @@
-import User from "../../../models/user.js";
-
-export const findUser = async(userInfoParams)=>{
-    const findUserResponse = await User.findOne({
-        where:{
-            user_email : `${userInfoParams.email}`,
-            provider : `${userInfoParams.provider}`
-        }
-    })
-    return findUserResponse;
+export const findUser = async(connection, userInfoParams)=>{ //사용자 조회
+    const findUserQuery = `
+    SELECT user_id, user_email, provider, COUNT(*) as count
+    FROM user
+    WHERE user_email = ?
+    AND provider = ?
+    GROUP BY user_id, user_email, provider;
+    `
+    const [findUserRow] = await connection.query(findUserQuery,userInfoParams);
+    return findUserRow[0];
 };
 
-export const createUser = async(userInfoParams) =>{
-    const createUserResponse = await User.create({
-        user_email : `${userInfoParams.email}`,
-        provider : `${userInfoParams.provider}`,
-    });
-    return createUserResponse;
+export const createUser = async(connection,userInfoParams) =>{ //사용자 등록
+    const createUserQuery = `
+    INSERT INTO user(user_email, provider)
+    VALUES(?,?);
+    `
+    const createUserRow = await connection.query(createUserQuery,userInfoParams);
+    return createUserRow[0];
 }
