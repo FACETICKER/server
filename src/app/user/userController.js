4fd9c0233958d00181e09b35c4999ccb20fe95df
한승헌 <seungheon123@khu.ac.kr>
2023-07-28 23:45:05 +0900

    Add: 스티커 요소 조회

--- src/app/user/userController.js
+++ src/app/user/userController.js
@@ -108,7 +108,7 @@ export const stickerController = {
                 user_id : user_id,
             };
             const stickersResult = await stickerService.getStickersByType(params); //호스트와 방문자를 구분해주기 위해
-            return res.send(stickersResult);
+            return res.status(200).send(stickersResult);
     
         }catch(err){
             return res.status(500).json(err);
@@ -154,13 +154,21 @@ export const stickerController = {
     attachSticker : async(req,res)=>{ //스티커 위치 저장
         try{
             const {x,y} = req.body;
-            const id = req.query.id; // 이 부분은 라우팅 경로가 nickname >> user_id로 바뀌어도 달라질 코드가 없는 게 맞나요?!?!?!
+            const id = req.query.user_id; // 이 부분은 라우팅 경로가 nickname >> user_id로 바뀌어도 달라질 코드가 없는 게 맞나요?!?!?!
             const params = [x,y,id]; 
             console.log(params);
             const result = await stickerService.insertStickerLocation(params);
             return res.send(result);
         }catch(err){
             return res.send(err);
+        }
+    },
+    getInfo : async(req,res) =>{
+        try{
+            const result = await stickerProvider.retrieveStickerInfo();
+            return res.send(response(baseResponse.SUCCESS,result));
+        }catch(err){
+            return res.status(500).send(err);
         }
     }
 };
@@ -278,9 +286,13 @@ export const nqnaController = {
 
 export const mainController = {
     getAll : async(req,res) =>{
-        const userIdFromJWT = req.verifiedToken ? req.verifiedToken.user_id : null; // 토큰이 있을 때만 user_id를 가져오도록 수정
-        const user_id = req.params.user_id;
-        const result = await mainpageService(userIdFromJWT,user_id);
-        return res.send(result);
+        try{
+            const userIdFromJWT = req.verifiedToken ? req.verifiedToken.user_id : null; // 토큰이 있을 때만 user_id를 가져오도록 수정
+            const user_id = req.params.user_id;
+            const result = await mainpageService(userIdFromJWT,user_id);
+            return res.status(200).send(result);
+        }catch(err){
+            return res.status(500).send(errResponse(baseResponse.SERVER_ERROR));
+        }
     }
 }
