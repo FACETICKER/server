84809e1a24f0c2dd9d9ac5f4e4def4efaad5c42f
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-07-28 15:55:51 +0900

    Update: 라우팅 경로명

--- src/app/user/userRoute.js
+++ src/app/user/userRoute.js
@@ -10,23 +10,22 @@ userRouter.get('/auth/google/callback',loginController.google); //구글 로그�
 
 // 페이지 관련
 userRouter.get('/:user_id',jwtMiddleware,mainController.getAll); //메인 페이지 조회
-userRouter.patch('/:user_id/sticker_message',jwtMiddleware,stickerController.postMessage); //스티커 메세지 등록(Host, Visitor) 
-userRouter.get('/:user_id/visitor_sticker/:visitor_sticker_id',stickerController.getSticker); //방문자 기록 visitor_sticker_id로 상세 조회 API
-userRouter.get("/:user_id/all_stickers",jwtMiddleware,stickerController.getStickers); //방문자 기록 페이지 조회
+userRouter.patch('/:user_id/sticker/message',jwtMiddleware,stickerController.postMessage); //스티커 메세지 등록(Host, Visitor) 
+userRouter.get('/:user_id/sticker/visitor/:visitor_sticker_id',stickerController.getSticker); //방문자 기록 visitor_sticker_id로 상세 조회 API
+userRouter.get("/:user_id/sticker/all",jwtMiddleware,stickerController.getStickers); //방문자 기록 페이지 조회
 
 // 스티커 관련
 // userRouter.post('/:user_id/sticker',stickerController.hostSticker); //Host 스티커 등록
 // userRouter.post('/:user_id/stickers/new',jwtMiddleware,stickerController.visitorSticker); //Visitor 스티커 등록
 userRouter.post('/:user_id/sticker',jwtMiddleware,stickerController.postSticker); //스티커 등록(Host,Visitor)
-userRouter.patch('/:user_id/attach_sticker',jwtMiddleware,stickerController.attachSticker); //스티커 부착, 스티커 위치 수정도 같은 API 호출
+userRouter.patch('/:user_id/sticker/attach',jwtMiddleware,stickerController.attachSticker); //스티커 부착, 스티커 위치 수정도 같은 API 호출
 
 
 // nQnA 관련
-userRouter.post('/:user_id/default_q',nqnaController.postDefaultQuestion); //default 질문 등록 API
-userRouter.post('/:user_id/visitor_q',nqnaController.postVisitorQuestion); //visitor 질문 등록 API
+userRouter.post('/:user_id/nQnA/default',nqnaController.postDefaultQuestion); //default 질문 등록 API
+userRouter.post('/:user_id/nQnA/visitor',nqnaController.postVisitorQuestion); //visitor 질문 등록 API
 userRouter.patch('/:user_id/nQnA/:nQnA_id/answer',nqnaController.postAnswer); //Host 답변 등록 API
 userRouter.get('/:user_id/nQnA',jwtMiddleware,nqnaController.getnQnA); //N문 N답 조회 API
-
 
 
 export default userRouter;
