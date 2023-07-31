7bebffb4674b5747debfb2a586c90c689732135d
한승헌 <seungheon123@khu.ac.kr>
2023-07-31 15:15:16 +0900

    Update: 서버 확인용

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ dotenv.config();
 
 app.listen(process.env.SERVER_PORT, ()=>{
     console.log(`server is ready, ${process.env.SERVER_PORT}`);
-    console.log(`${process.env.SERVER_PORT}`);
-    console.log(`${process.env.DB_HOST}, ${process.env.DB_NAME}, ${process.env.DB_PASS}, ${process.env.DB_USER}`);
 });
