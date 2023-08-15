04032b26b816db05f69f9f248d276ac262672d13
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-08-15 15:45:45 +0900

    Update: 스티커 부착 수정

--- src/app/user/userController.js
+++ src/app/user/userController.js
@@ -152,11 +152,11 @@ export const stickerController = {
     attachSticker : async(req,res)=>{ //스티커 위치 저장
         try{
             const {x,y} = req.body;
-            const id = req.query.user_id; // 이 부분은 라우팅 경로가 nickname >> user_id로 바뀌어도 달라질 코드가 없는 게 맞나요?!?!?!
+            const id = req.query.id; // 이 부분은 라우팅 경로가 nickname >> user_id로 바뀌어도 달라질 코드가 없는 게 맞나요?!?!?!
             const params = [x,y,id]; 
             const result = await stickerService.insertStickerLocation(params);
             return res.send(result);
-        }catch(err){
+        }catch(err){    
             return res.send(err);
         }
     },
